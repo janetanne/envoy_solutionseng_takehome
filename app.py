60d1222bf9ac12d956f9d6949b0f995d0cc87779
Janet Anne Panen <37631991+janetanne@users.noreply.github.com>
2025-07-17 11:30:11 -0700

    modified settings to remove print statements, add webhook handling

--- app.py
+++ app.py
@@ -25,73 +25,57 @@ config = {
     "allowed_minutes": 180
 }
 
-@app.route('/settings', methods=['GET', 'POST'])
+@app.route('/settings', methods=['POST'])
 def settings():
-    print("/settings was hit")
-    
-    if request.method == 'POST':
-        data = request.get_json()
-        print("post request: ", data)
+    data = request.get_json()
 
-        # extract payload from request, check that it's a number
-        try:
-            input_value = data.get("payload", {}).get("allowed_minutes")
-            # converts input value to int, removes whitespace
-            minutes = int(str(input_value).strip())
-        except (ValueError, TypeError):
-            return jsonify({"error": "Value must be a number"}), 400
+    # extract payload from request, validate that it's a number
+    try:
+        input_value = data.get("payload", {}).get("allowed_minutes")
+        # converts input value to int, removes whitespace
+        minutes = int(str(input_value).strip())
+    except (ValueError, TypeError):
+        return jsonify({"error": "Value must be a number"}), 400
 
-        # validate that it's between 0-180
-        if 0 <= minutes <= 180:
-            config["allowed_minutes"] = minutes
-            return jsonify({
-                "allowed_minutes": {
-                    "value": str(config["allowed_minutes"])
-                }
+    # validate that it's between 0-180
+    if 0 <= minutes <= 180:
+        config["allowed_minutes"] = minutes
+        return jsonify({
+            "allowed_minutes": {
+                                    "value": str(config["allowed_minutes"])
+                               }
             }), 200
-        else:
-            return jsonify({"error": "Allowed minutes must be between 0 and 180 minutes"}), 400
+    else:
+        return jsonify({"error": "Allowed minutes must be between 0 and 180 minutes"}), 400
+
 
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
-    print("webhook endpoint hit")
+    print("********** webhook endpoint hit ***********")
     event = request.get_json()
 
-    # parse needed info
+    # parse needed event type
     event_type = event.get("meta", {}).get("event")
+
+    # check config from installer setup JUST IN CASE
+    config = event.get("meta", {}).get("config", {})
+    allowed_input = config.get("allowed_minutes", {}).get("value", "180") # fallback to 180 if config isn't present
+
+    if allowed_input and allowed_input.strip().isdigit():
+        allowed_minutes = int(allowed_input.strip())
+    
+    else: 
+        allowed_minutes = 180
+    
+    # parse data from event
     attributes = event.get("payload", {}).get("attributes", {})
     full_name = attributes.get("full-name", "N/A") # defaults to N/A if name isn't present
 
-    print(f"RECEIVED EVENT: {event_type} for {full_name}")
+    if event_type == "entry_sign_in":
+        return jsonify({"message": f"Sign-in logged for {full_name}"}), 200
 
-    
+    if event_type == "entry_sign_out":
 
-
-    return "200 OK", 200
-
-    # try:
-
-    #     attributes = event["data"]["attributes"]
-    #     signed_in_at = attributes["signed-in-at"]
-    #     signed_out_at = attributes["signed-out-at"]
-
-    #     # this converts times from iso into datetime object 
-    #     sign_in_time = datetime.fromisoformat(signed_in_at.rstrip("Z"))
-    #     sign_out_time = datetime.fromisoformat(signed_out_at.rstrip("Z"))
-
-    #     # this calculates the duration
-    #     duration = int((sign_out_time - sign_in_time).total_seconds() / 60)
-    #     allowed = config["allowed_minutes"]
-
-    #     if duration > allowed:
-    #         message = f"Visitor overstayed by {duration - allowed} minutes."
-    #     else:
-    #         message = "Visitor left on time."
-
-    #     return jsonify({"message": message}), 200
-
-    # except Exception as e:
-    #     return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5050, debug=True)
